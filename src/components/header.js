c2a03f2466466e9ee7a74f4030a89a77e5951bf0
Revulse3 <dustynfoster@aol.com>
2018-11-28 18:39:23 -0700

    finished project

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
+            <div className="header__skew"></div>
+            <div className="header__skewTwo"></div>
             <h1>Rev's Mad libs</h1>
             <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
         </div>
